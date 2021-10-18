41459fd1ba6d9a685da16e9d88a3f79ebac0a0f7
lege0053 <marine.legendre@etudiant.univ-reims.fr>
2021-10-18 20:10:00 +0200

    Modification des noms de pages

--- profile_change_password.php
+++ profile_change_password.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 require "autoload.php";
 Session::start();
 
-$webPage = new WebPage("Activités");
+$webPage = new WebPage("Changement de mot de passe");
 $webPage->appendToHead(<<< HTML
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     HTML);
