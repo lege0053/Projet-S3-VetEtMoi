510808446538d4feacd80a6a2625e96261cf5a88
Marine Legendre <marine.legendre@etudiant.univ-reims.fr>
2021-11-08 17:46:46 +0100

    changement de mail opérationnel.

--- profile_change_mail.php
+++ profile_change_mail.php
@@ -11,7 +11,7 @@ $form = <<<HTML
 <div class="d-flex flex-row justify-content-center mb-5">
     <img src="img/animal/dog1.png" height="250px" class="align-self-center mr-5"/>
     <div class="d-flex flex-column w-50 pt-2 pb-2 pr-5 pl-5" style="background-color: #DDDDDD; border-radius: 10px">
-            <form action="trmt/connexion_trmt.php" method="post">   
+            <form action="trmt/change_mail.php" method="post">   
                 <div class="d-flex pb-4 mt-2 justify-content-center">
                     <div class="d-flex flex-row ">
                         {$webPage->getIcon("mail")}
@@ -23,21 +23,21 @@ $form = <<<HTML
                         {$webPage->getIcon("mail")}
                         <div style="font-weight: bold;">Adresse mail actuelle</div>
                     </div>
-                    <input type="email" name="mail" class="pt-1 pb-1 pr-2 pl-2 rounded" style="outline: 0; border:0;background-color: #C9C9C9;" placeholder="Votre Adresse mail actuelle" required>
+                    <input type="email" name="oldMail" id="oldMail" class="pt-1 pb-1 pr-2 pl-2 rounded" style="outline: 0; border:0;background-color: #C9C9C9;" placeholder="Votre Adresse mail actuelle" required>
                 </div>
                 <div class="form-group d-flex flex-column">
                     <div class="d-flex flex-row">
                         {$webPage->getIcon("mail")}
                         <div style="font-weight: bold;">Nouvelle Adresse mail</div>
                     </div>
-                    <input type="email" name="mail" class="pt-1 pb-1 pr-2 pl-2 rounded" style="outline: 0; border:0;background-color: #C9C9C9;" placeholder="Nouvelle Adresse mail" required>
+                    <input type="email" name="newMail" id="newMail" class="pt-1 pb-1 pr-2 pl-2 rounded" style="outline: 0; border:0;background-color: #C9C9C9;" placeholder="Nouvelle Adresse mail" required>
                 </div>
                 <div class="form-group d-flex flex-column">
                     <div class="d-flex flex-row">
                         {$webPage->getIcon("mail")}
                         <div style="font-weight: bold;">Répéter votre Nouvelle Adresse mail</div>
                     </div>
-                    <input type="email" name="mail" class="pt-1 pb-1 pr-2 pl-2 rounded" style="outline: 0; border:0;background-color: #C9C9C9;" placeholder="Répéter votre Nouvelle Adresse mail" required>
+                    <input type="email" name="repeatNewMail" id="repeatNewMail" class="pt-1 pb-1 pr-2 pl-2 rounded" style="outline: 0; border:0;background-color: #C9C9C9;" placeholder="Répéter votre Nouvelle Adresse mail" required>
                 </div>
                 <div class="d-flex flex-row justify-content-center">
                     <div class="form-group d-inline-flex mt-3">
--- trmt/change_mail.php
+++ trmt/change_mail.php
@@ -1,2 +1,33 @@
 <?php
 declare(strict_types=1);
+
+include_once "../autoload.php";
+
+$auth = new SecureUserAuthentication();
+if($auth->isUserConnected()){
+    $user = $auth->getUser();
+
+    if(isset($_POST['oldMail'], $_POST['newMail'], $_POST['repeatNewMail'])
+        && !empty($_POST['oldMail']) && !empty($_POST['newMail']) && !empty($_POST['repeatNewMail'])){
+        $oldMail = WebPage::escapeString($_POST['oldMail']);
+        $newMail = WebPage::escapeString($_POST['newMail']);
+        $repeatNewMail = WebPage::escapeString($_POST['repeatNewMail']);
+
+        $req = MyPDO::getInstance()->prepare(<<<SQL
+            SELECT * FROM Users
+            WHERE email = :mail
+        SQL);
+        $req->execute(['mail' => $newMail]);
+        if(!$req->rowCount() > 0){
+            if($newMail === $repeatNewMail){
+                $req2 = MyPDO::getInstance()->prepare(<<<SQL
+                    UPDATE Users
+                    SET email= :mail 
+                    WHERE userId = :userId;
+                SQL);
+                $req2->execute(['userId' => $user->getUserId(), 'mail' => $newMail]);
+                header('Location: ../profile.php');
+            } else header('Location: ../profile_change_mail.php?err_repeatMail');
+        } else header('Location: ../profile_change_mail.php?err_newMail_already_exist');
+    } else header('Location: ../profile_change_mail.php?err_infos');
+} else header('Location: ../connexion.php');
