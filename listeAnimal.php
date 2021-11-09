8872642fc7da7b8ab05137f0835e87aaa4cec2b4
Marine Legendre <marine.legendre@etudiant.univ-reims.fr>
2021-11-09 15:58:36 +0100

    Modification du visuel de la page

--- listeAnimal.php
+++ listeAnimal.php
@@ -29,23 +29,22 @@ HTML);
 
     foreach ($rep as $animal) {
     $webPage->appendContent(<<< HTML
-<div class="d-flex flex-row justify-content-around pt-2 pb-2 pr-5 pl-5" style="background-color: #DDDDDD; border-radius: 10px; width: 80%">
-    <div>
-        <img src="img/partChien.png" class="mx-auto w-25" alt="">
-    </div>
-    <div class="d-flex flex-column justify-content-start">
-        <a style="color: #02897A; font-weight: bold;">Prochain rendez-vous
-        <a style="color: #262626; font-weight: bold;">Mercredi 2 Octobre 2021
-        <a style="color: #262626; font-weight: bold;">11:00
-        <a style="color: #02897A; font-weight: bold;">Dernier rendez-vous
-        <a style="color: #262626; font-weight: bold;">Jeudi 25 Octobre 2020
-    </div>
-    <div class='d-flex flex-column justify-content-center'>
-        <a class='button' href='#'>Info sur {$animal['name']}</a>
-        <a class='button' href="#">Rendez-Vous</a>
+<div class="d-flex flex-row justify-content-center">
+    <div class="container justify-content-space-around row w-50 m-3 p-3" style="background-color: #DDDDDD;border-radius: 15px;">
+        <img src="img/partChien.png" alt="" style="height: 200px;">
+        <div class="d-flex flex-column justify-content-center">
+            <a style="color: #02897A; font-weight: bold;">Prochain rendez-vous
+            <a style="color: #262626; font-weight: bold;">Mercredi 2 Octobre 2021
+            <a style="color: #262626; font-weight: bold; margin-bottom: 10px;">11:00
+            <a style="color: #02897A; font-weight: bold;">Dernier rendez-vous
+            <a style="color: #262626; font-weight: bold;">Jeudi 25 Octobre 2020
+        </div>
+        <div class='d-flex flex-column justify-content-center'>
+            <a class='button m-1 p-2' href='#'>Info sur {$animal['name']}</a>
+            <a class='button m-1 p-2' style="text-align: center" href="#">Rendez-Vous</a>
+        </div>
     </div>
 </div>
-
 HTML);
 }
 
