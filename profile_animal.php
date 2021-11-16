3d76b1225ab40987f1677944acb242b73f5fda78
Marine Legendre <marine.legendre@etudiant.univ-reims.fr>
2021-11-16 18:06:18 +0100

    Modification du visuel du profil de l'animal.

--- profile_animal.php
+++ profile_animal.php
@@ -31,28 +31,26 @@ $html= <<< HTML
     <h3 style="font-weight: bold;">{$webPage->getIcon('cat', 35)}Le profil de {$animal->getName()}</h3>
 </div>
 
-<div class="d-flex flex-row justify-content-center" style="background-color: #DDDDDD; border-radius: 10px">
-    {$webPage->getImgCarre("dog", "", 300)}
-    <div class="d-flex flex-column justify-content-left">
-        <div class="d-flex flex-column justify-content-left"> 
-            <a style="color: #02897A; font-weight: bold;">Nom
-            <a style="color: #262626; font-weight: bold;">{$animal->getName()}
+<div class="d-flex flex-row justify-content-center">
+    <div class="d-flex justify-content-space-between row p-3" style="background-color: #DDDDDD;border-radius: 15px; width:60%">
+        <div class="d-flex flex-row">
+            {$webPage->getImgCarre("{$animal->getNameImgCarre()}", "{$animal->getName()}", 300)}
+            <div class="d-flex flex-column justify-content-center" style="margin-left: 10px; margin-right: 150px; font-size: 20px;">
+                    <a style="color: #02897A; font-weight: bold;">Nom
+                    <a style="color: #262626; font-weight: bold; margin-bottom: 20px;">{$animal->getName()}
+                    <a style="color: #02897A; font-weight: bold;">Age
+                    <a style="color: #262626; font-weight: bold;margin-bottom: 20px;">{$age}
+                    <a style="color: #02897A; font-weight: bold;">Date de Naissance
+                    <a style="color: #262626; font-weight: bold;">{$animal->getBirthDay()}
+            </div>
         </div>
-        <div class="d-flex flex-column justify-content-left">
-            <a style="color: #02897A; font-weight: bold;">Age
-            <a style="color: #262626; font-weight: bold;">{$age}
+        <div class="d-flex flex-column justify-content-center" style="font-size: 20px;">
+            <a style="color: #02897A; font-weight: bold;">Prochain rendez-vous
+            <a style="color: #262626; font-weight: bold;">{$futurRDV}
+            <a style="color: #262626; font-weight: bold;margin-bottom: 20px;">11:00
+            <a style="color: #02897A; font-weight: bold;">Dernier rendez-vous
+            <a style="color: #262626; font-weight: bold;">{$dernierRDV}
         </div>
-        <div class="d-flex flex-column justify-content-left">
-            <a style="color: #02897A; font-weight: bold;">Date de Naissance
-            <a style="color: #262626; font-weight: bold;">{$animal->getBirthDay()}
-        </div>
-    </div>
-    <div class="d-flex flex-column justify-content-left">
-        <a style="color: #02897A; font-weight: bold;">Prochain rendez-vous
-        <a style="color: #262626; font-weight: bold;">{$futurRDV}
-        <a style="color: #262626; font-weight: bold;">11:00
-        <a style="color: #02897A; font-weight: bold;">Dernier rendez-vous
-        <a style="color: #262626; font-weight: bold;">{$dernierRDV}
     </div>
 </div>
 HTML;
