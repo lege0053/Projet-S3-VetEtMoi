7c0200c95a00030b53b794595dc1513d92d7455d
Marine Legendre <marine.legendre@etudiant.univ-reims.fr>
2021-11-23 12:41:02 +0100

    Ajout des rendez-vous dans le profil de l'animal

--- profile_animal.php
+++ profile_animal.php
@@ -18,19 +18,91 @@ $animal = Animal::createFromId($animalId);
 
 $age = date_diff(date_create($animal->getBirthDay()), date_create("now"))->format("%y ans %m mois");
 
-
 $webPage = new WebPage("Profil de {$animal->getName()}");
+$webPage->appendJsUrl("js/meetingUtils.js");
+$webPage->appendJsUrl("js/ajaxrequest.js");
+setlocale(LC_ALL, 'fr_FR', 'French_France', 'French');
+date_default_timezone_set('Europe/Paris');
+$webPage->appendCss(<<<CSS
+    .rdv-item{
+        background-color: #C9C9C9;
+        border-radius: 10px;
+        padding: 0px 0px 0px 25px;
+        margin: 10px;
+        display: flex;
+        align-items: center;
+    }
+    .rdv-item span {
+        flex: 1;
+    }
+    
+    .rdv-head{
+        background-color: #C9C9C9;
+        border-radius: 10px;
+        padding: 0px 0px 0px 25px;
+        margin: 10px;
+        display: flex;
+        flex-grow: 1;
+        align-items: center;
+        height: 50px;
+        font-size: 1.3em;
+    }
+    
+    .rdv-head span{
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .rdv {
+        display: flex;
+        flex-direction: column;
+        width: 900px;
+        background: #DDDDDD;
+        margin: 0;
+        padding: 25px;
+        border-radius: 20px;
+    }
+CSS
+);
 
-//test
-    $futurRDV = "Mercredi 2 Octobre";
-    $dernierRDV = "Jeudi 25 Octobre";
+//Meeting
+$rdvList = $animal->getAllMeetings();
+$rdvHTML = "<span>Vous n'avez aucun rendez-vous</span>";
+if($rdvList){
+    $rdvs = "";
+    foreach($rdvList as $rdv){
+        $meetingId = $rdv->getMeetingId();
+        $date = ucwords(utf8_encode(strftime("%A %d %b %Y - %H:%M", strtotime($rdv->getDateTime()))));
+        $rdvs .= "<div class='rdv-item'>
+                    <span>{$date}</span>
+                    <span style='display: flex; justify-content: end;'>
+                        <input type='button' class='button' onclick='showEditMeetingPopup(\"$meetingId\");' value='Modifier' style='padding: 12px 25px; font-size: 18px; '>
+                    </span>
+                    <!--<div style='display: flex;'>
+                        {$webPage->getHTMLButton(false, "Modifier", "profile_animal.php?id=$animalId", "12px", "25px", "18px")}
+                        <form action='trmt/delete_meeting_trmt.php' method='post' name='delete_meeting_{$rdv->getMeetingId()}' style='margin: 0; padding: 0;'>
+                            <input type='text' name='meetingId' value='{$rdv->getMeetingId()}' hidden>
+                            {$webPage->getHTMLButton(true, "Supprimer", "", "12px", "25px", "18px")}
+                        </form>
+                    </div>-->
+                  </div>";
+    }
+    $rdvHTML = <<< HTML
+        <div class="rdv">
+            <div class="rdv-head">
+                <span>Date</span>
+                <span></span>           
+                <span></span>  
+            </div>
+            $rdvs
+        </div>
+
+    HTML;
+}
 
 
 $html= <<< HTML
-<div class="d-flex justify-content-center">
-    <h3 style="font-weight: bold;">{$webPage->getIcon('cat', 35)}Le profil de {$animal->getName()}</h3>
-</div>
-
 <div class="d-flex flex-row justify-content-center">
     <div class="d-flex row p-3 align-items-center" style="background-color: #DDDDDD;border-radius: 15px;">
         {$webPage->getImgCarre("{$animal->getSpecieName()}", "{$animal->getName()}", 300)}
@@ -59,6 +131,16 @@ $html= <<< HTML
 </div>
 HTML;
 
-
-$webPage->appendContent($html);
+$webPage->appendContent(<<<HTML
+    <div class="d-flex flex-column">
+        <div class="d-flex justify-content-center">
+            <h1 class="title pb-2" style="font-weight: bold;">{$webPage->getIcon('cat', 35)}Le profil de {$animal->getName()}</h1>
+        </div>
+            $html
+        <div class="d-flex flex-column justify-content-center align-items-center pt-5">
+            <h1 class="title">Prochain Rendez-Vous</h1>
+            $rdvHTML
+        </div>
+    </div>
+HTML);
 echo $webPage->toHTML();
