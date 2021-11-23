f63791d7fa1834797b28ea959010115a1656ce57
Nico <nicolas.gay@etudiant.univ-reims.fr>
2021-11-23 21:22:52 +0100

    Ajout dans de getVetoList dans api/ permettant de récupérer la liste de tous les vétérinaires

---
+++ api/getVetoList.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+include_once "../src/MyPDO.php";
+
+header('Content-type: application/json');
+
+$rq = MyPDO::getInstance()->prepare(<<<SQL
+    SELECT userId, firstName, lastName FROM Users
+    WHERE isVeto = 1;
+SQL);
+$rq->execute();
+$array = $rq->fetchAll();
+if($array)
+    echo json_encode($array);
+else
+    echo json_encode(["error" => "no_veto_found"]);
