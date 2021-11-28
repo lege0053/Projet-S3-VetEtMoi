e821443ec0fd6cc6fa563effdf3c41d4303a7e2b
lege0053 <marine.legendre@etudiant.univ-reims.fr>
2021-11-29 00:30:05 +0100

    Ajout de l'agenda présentant les crénaux disponible v.1

--- prisederdv.php
+++ prisederdv.php
@@ -48,15 +48,88 @@ foreach($reqVeto as $veto)
 
 $submitButton=$webPage->getHTMLButton(true, 'Valider');
 
-$html= <<< HTML
-<div class="d-flex justify-content-center">
-    <h3 style="font-weight: bold;">$h3</h3>
+//PLANNING
+$planning = <<<HTML
+<style>
+    table {
+      border-collapse: collapse;
+      letter-spacing: 1px;
+      font-size: 0.8rem;
+    }
+
+    td, th {
+      padding: 10px 20px;
+      text-align: center;
+    }
+    
+    th {
+    font-weight: bold;
+    }
+    
+    .buttonHr {
+    border: none;
+    border-radius: 10px;
+    padding: 20px;
+    font: inherit;
+    line-height: 1;
+    width: 150px;
+    }
+    
+    .buttonHr:hover,
+    .buttonHr:focus {
+    transform: translateY(-0.25em);
+    
+}
+</style>
+
+<div class="d-flex flex-column">
+    <div class="d-flex justify-content-center" style="background-color: #242424; color: white; padding-top: 10px; padding-bottom: 10px; border-radius: 10px;">Du 01/10/2021 au 06/10/2021</div>
+    <table>
+        <colgroup>
+            <col style="background-color:#C7C7C7;">
+            <col span="1">
+            <col style="background-color:#C7C7C7;">
+            <col span="1">
+            <col style="background-color:#C7C7C7;">
+        </colgroup>
+        <thead>
+            <tr>
+                <th scope="col">Lundi</th>
+                <th scope="col">Mardi</th>
+                <th scope="col">Mercredi</th>
+                <th scope="col">Jeudi</th>
+                <th scope="col">Vendredi</th>
+                <th scope="col">Samedi</th>
+            </tr>
+        </thead>
+        <tbody>
+HTML;
+
+$horaires = ['8h00 - 9h00', '9h00 - 10h00', '10h00 - 11h00', '11h00 - 12h00', '12h00 - 13h00', '13h00 - 14h00', '14h00 - 15h00', '15h00 - 16h00', '16h00 - 17h00', '17h00 - 18h00', '18h00 - 19h00'];
+$color  = "#81CB45";
+foreach ($horaires as $horaire)
+{
+    $planning .= "<tr>";
+    for ($i=0; $i<6; $i++)
+    {
+        $planning .= "<td><button class='buttonHr' type='submit' value='' style='background: $color;'>$horaire</button></td>";
+    }
+    $planning.="</tr>";
+}
+$planning.= <<< HTML
+    </tbody>
+</table>
 </div>
-<div>
+HTML;
+//FIN PLANNING
+
+$html= <<< HTML
+<div class="d-flex flex-column justify-content-center">
+    <h3 style="font-weight: bold; align-self: center">$h3</h3>
     <form action="./trmt/prisederdv_trmt.php" method="post">
         <select name="veto" id="veto" required>$optionsVeto</select>
         $conditionalSelect
-        <!--AjouterTableau-->
+        $planning
         $submitButton
     </form>
 </div>
