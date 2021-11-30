d640acc9551bc483817f2e23945e83851bc9c372
Nico <nicolas.gay@etudiant.univ-reims.fr>
2021-11-30 16:55:50 +0100

    Ajout du script permettant de changer de date dans la page de prise de rendez-vous

--- prisederdv.php
+++ prisederdv.php
@@ -9,6 +9,32 @@ if(!SecureUserAuthentication::isUserConnected())
 
 $user = $auth->getUser();
 $webPage = new WebPage("Prise de Rendez-Vous");
+
+$hiddenAnimalId = "";
+if(isset($_POST['animalId']) && !empty($_POST['animalId'])) {
+    $animalId = $_POST['animalId'];
+    $hiddenAnimalId = "<input id='hiddenAnimalId' type='text' value='$animalId' hidden>";
+}
+
+
+
+// Selects bar //
+
+$vetoList = User::getVetoList();
+$vetoOptions = "";
+foreach ($vetoList as $veto){
+    $vetoOptions .= "<option value='{$veto['userId']}'>{$veto['lastName']} {$veto['firstName']}</option>{}";
+}
+
+$speciesList = Species::getSpeciesList();
+$speciesOptions = "<option value=''>Veuillez choisir une espèce</option>";
+foreach ($speciesList as $species){
+    $speciesOptions .= "<option value='{$species->getSpeciesId()}'>{$species->getSpeciesName()}</option>{}";
+}
+$animalsSelect = "<select id='animalId' name='animalId'><option value='-1'>Nouvel Animal</option></select>";
+$speciesSelect = "<select id='speciesId' name='speciesId'>$speciesOptions</select>";
+$vetosSelect = "<select id='vetoId' name='vetoId'>$vetoOptions</select>";
+
 $webPage->appendCss(<<<CSS
     table {
       border-collapse: collapse;
@@ -31,38 +57,30 @@ $webPage->appendCss(<<<CSS
         padding: 20px;
         font: inherit;
         line-height: 1;
-        width: 150px;
+        width: 130px;
+        border-bottom: solid rgba(0,0,0,0) 0.25em;
+        transition: transform 0.1s ease-in-out, border-bottom 0.2s ease-in-out;
     }
     
     .buttonHr:hover,
     .buttonHr:focus {
         transform: translateY(-0.25em);
+        border-bottom: solid #639c35 0.25em;
+    }
+
+    .tableTitle {
+        flex: 1;
+        flex-grow: 1;
+        text-align: center;
+        padding: 20px 0 20px 0;
+    }
+    .tableTitle:nth-child(2n+1){
+        background-color:#C7C7C7;
+    }
+    .tableTitle:nth-child(2n){
+        background-color:#dedede;
     }
 CSS);
-
-$hiddenAnimalId = "";
-if(isset($_POST['animalId']) && !empty($_POST['animalId'])) {
-    $animalId = $_POST['animalId'];
-    $hiddenAnimalId = "<input id='hiddenAnimalId' type='text' value='$animalId' hidden>";
-}
-
-/////////////////
-// Selects bar //
-/////////////////
-$vetoList = User::getVetoList();
-$vetoOptions = "<option value=''>Vétérinaire</option>";
-foreach ($vetoList as $veto){
-    $vetoOptions .= "<option value='{$veto['userId']}'>{$veto['lastName']} {$veto['firstName']}</option>{}";
-}
-
-$speciesList = Species::getSpeciesList();
-$speciesOptions = "<option value=''>Veuillez choisir une espèce</option>";
-foreach ($speciesList as $species){
-    $speciesOptions .= "<option value='{$species->getSpeciesId()}'>{$species->getSpeciesName()}</option>{}";
-}
-$animalsSelect = "<select id='animalId' name='animalId'><option value='-1'>Nouvel Animal</option></select>";
-$speciesSelect = "<select id='speciesId' name='speciesId'>$speciesOptions</select>";
-$vetosSelect = "<select id='vetoId' name='vetoId'>$vetoOptions</select>";
 
 $webPage->appendJs(<<<JS
 
@@ -73,6 +91,56 @@ $webPage->appendJs(<<<JS
         let animalSelect = document.getElementById('animalId');
         let speciesSelect = document.getElementById('speciesId');
         let vetoSelect = document.getElementById('vetoId');
+        let dateBefore = document.getElementById('dateBefore');
+        let dateAfter = document.getElementById('dateAfter');
+        
+        let currentDay = new Date();
+        let first = currentDay.getDate() - currentDay.getDay() + 1;
+        let last = first + 6;
+        let firstDay = new Date(currentDay.setDate(first));
+        let lastDay = new Date(currentDay.setDate(last));
+        
+        let spanFirstDay = document.getElementById('startDate');
+        spanFirstDay.innerText = firstDay.toLocaleDateString();
+        let spanLastDay = document.getElementById('endDate');
+        spanLastDay.innerText = lastDay.toLocaleDateString();
+        
+        let year = firstDay.getFullYear();
+        var oneJan = new Date(firstDay.getFullYear(),0,1);
+        var numberOfDays = Math.floor((firstDay - oneJan) / (24 * 60 * 60 * 1000));
+        var week = Math.ceil(( firstDay.getDay() + 1 + numberOfDays) / 7);
+        
+        dateBefore.onclick = function() {
+            backward();
+            updateDate();
+        }
+        
+        dateAfter.onclick = function() {
+            forward();
+            updateDate();
+        }
+        
+        function forward(){
+            firstDay = new Date(firstDay.setDate(firstDay.getDate() + 7));
+            lastDay = new Date(lastDay.setDate(lastDay.getDate() + 7));
+        }
+        function backward() {
+            firstDay = new Date(firstDay.setDate(firstDay.getDate() - 7));
+            lastDay = new Date(lastDay.setDate(lastDay.getDate() - 7));
+        }
+        function updateDate(){
+            spanFirstDay.innerText = firstDay.toLocaleDateString();
+            spanLastDay.innerText = lastDay.toLocaleDateString();
+            
+            year = firstDay.getFullYear();
+            let oneJan = new Date(firstDay.getFullYear(),0,1);
+            let numberOfDays = Math.floor((firstDay - oneJan) / (24 * 60 * 60 * 1000));
+            week = Math.ceil(( firstDay.getDay() + 1 + numberOfDays) / 7);
+            
+            console.log(week);
+        }
+        
+        reloadAvailableTimeSlot();
         
         new AjaxRequest(
         {
@@ -91,7 +159,6 @@ $webPage->appendJs(<<<JS
                     animalList[res[animal]['animalId']] = res[animal]['speciesId'];
                     animalSelect.appendChild(option);
                 }
-                console.log(animalList);
             },
             onError: function (status, message) {
             }
@@ -133,33 +200,49 @@ $webPage->appendJs(<<<JS
                     meetingDiv.removeChild(meetingDiv.firstChild);
                 }
             }
-            console.log("REMOVE");
         }
+        
         
         function addAvailableTimeSlot(res){
             for(let i = 0; i < res.length; i++){
-                let radio = document.createElement('radio');
+                let radio = document.createElement('span');
                 radio.className = 'buttonHr';
                 radio.name = 'timeSlotId';
                 radio.value = res[i]['timeSlotId'];
                 radio.style.background = '#81CB45';
-                radio.innerText = res[i]['startHour'];
+                radio.innerText = res[i]['startHour'].substring(0, res[i]['startHour'].length - 3);
+                radio.style.textAlign = 'center';
+                radio.onclick = function() {
+                    radioOnClick();
+                }
                 
-                console.log(res[i]['dayName']);
                 let div = document.getElementById(res[i]['dayName']);
                 document.getElementById(res[i]['dayName']).appendChild(radio);
             }
+            
+            let ids = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+            for(let id in ids){
+                let meetingDiv = document.getElementById(ids[id]);
+                if(!meetingDiv.firstChild){
+                    let span = document.createElement('span');
+                    meetingDiv.appendChild(span);
+                }
+            }
         }
+        
+        
+        function radioOnClick()
+        {
+            
+        }
+        
+        
         
         function reloadAvailableTimeSlot() {
             let meetingType = document.querySelector('input[name="timeSlotTypeId"]:checked').id;
             let vetoId = vetoSelect.options[vetoSelect.selectedIndex].value;
             let year = '2021';
             let week = '48';
-            console.log(meetingType);
-            console.log(vetoId);
-            console.log(year);
-            console.log(week);
             new AjaxRequest({
                 url: "api/getAvailableTimeSlot.php",
                 method: 'get',
@@ -173,7 +256,6 @@ $webPage->appendJs(<<<JS
                 onSuccess: function (res) {
                     clearAvailableTimeSlot();
                     addAvailableTimeSlot(res);
-                    console.log(res);
                 },
                 onError: function (status, message) {
                 }  
@@ -183,34 +265,8 @@ $webPage->appendJs(<<<JS
     }
 JS);
 
-//PLANNING
-$planning = <<<HTML
-<div class="d-flex flex-column" style="width: 800px;">
-    <div class="d-flex justify-content-center" style="background-color: #242424; color: white; padding-top: 10px; padding-bottom: 10px; border-radius: 10px;">Du 01/10/2021 au 06/10/2021</div>
-    
-    <div class="d-flex flex-column justify-content-center align-items-center" style="margin-right: 25px; margin-left: 25px;">
-         <div class="d-flex justify-content-center align-items-center" style="flex: 1; flex-grow: 1; width: 100%;">
-            <span style="background-color:#C7C7C7; flex: 1; flex-grow: 1; text-align: center;">Lundi</span>
-            <span style="flex: 1; flex-grow: 1; text-align: center;">Mardi</span>
-            <span style="background-color:#C7C7C7; flex: 1; flex-grow: 1; text-align: center;">Mercredi</span>
-            <span style="flex: 1; flex-grow: 1; text-align: center;">Jeudi</span>
-            <span style="background-color:#C7C7C7; flex: 1; flex-grow: 1; text-align: center;">Vendredi</span>
-            <span style="flex: 1; flex-grow: 1; text-align: center;">Samedi</span>
-        </div>
-        <div class="d-flex justify-content-center align-items-center">
-            <div id="Lundi" style="background-color:#C7C7C7;"></div>
-            <div id="Mardi"></div>
-            <div id="Mercredi" style="background-color:#C7C7C7;"></div>
-            <div id="Jeudi"></div>
-            <div id="Vendredi"></div>
-            <div id="Samedi"></div>
-        </div>
-    </div>
-</div>
-HTML;
-//FIN PLANNING
+$webPage->appendContent(<<<HTML
 
-$pageContent = <<< HTML
 <div class="d-flex flex-column justify-content-center align-items-center">
     <span class="title main-ui-class" style="margin-top: 50px; margin-bottom: 30px;">Prise de Rendez-Vous</span>
     <div class="d-flex justify-content-center align-items-center" style="margin: 1em; column-gap: 2em;">
@@ -237,9 +293,42 @@ $pageContent = <<< HTML
             </div>
         </div>
     </div>
-    $planning
-</div>
-HTML;
 
-$webPage->appendContent($pageContent);
+    <!-- Planning -->
+    
+    <div class="d-flex flex-column" style="width: 1000px;">
+        <div id="dateNavBar" class="d-flex justify-content-center align-items-center" style="background-color: #242424; color: white; padding-top: 10px; padding-bottom: 10px; border-radius: 10px; column-gap: 20px;">
+            <span id="dateBefore">{$webPage->getIcon('arrow-left', 28)}</span>
+            <span id="date" style="display: flex; color: white; column-gap: 20px;">
+                Du
+                <span id="startDate" style="color: white;">??/??/????</span>
+                au
+                <span id="endDate" style="color: white;">??/??/????</span>
+            </span>
+            <span id="dateAfter">{$webPage->getIcon('arrow-right', 28)}</span>
+        </div>
+        
+        <div class="d-flex flex-column justify-content-center align-items-center" style="margin-right: 25px; margin-left: 25px;">
+             <div class="d-flex justify-content-center align-items-center" style="flex: 1; flex-grow: 1; width: 100%">
+                <span class="tableTitle">Lundi</span>
+                <span class="tableTitle">Mardi</span>
+                <span class="tableTitle">Mercredi</span>
+                <span class="tableTitle">Jeudi</span>
+                <span class="tableTitle">Vendredi</span>
+                <span class="tableTitle">Samedi</span>
+            </div>
+            <div class="d-flex justify-content-center" style="flex: 1; flex-grow: 1; width: 100%;">
+                <div style="padding-bottom: 1em; background-color:#C7C7C7; flex: 1; flex-grow: 1; row-gap: 0.5em;" class="d-flex flex-column align-items-center" id="Lundi"></div>
+                <div style="padding-bottom: 1em; background-color:#dedede; flex: 1; flex-grow: 1; row-gap: 0.5em;" class="d-flex flex-column align-items-center" id="Mardi"></div>
+                <div style="padding-bottom: 1em; background-color:#C7C7C7; flex: 1; flex-grow: 1; row-gap: 0.5em;" class="d-flex flex-column align-items-center" id="Mercredi"></div>
+                <div style="padding-bottom: 1em; background-color:#dedede; flex: 1; flex-grow: 1; row-gap: 0.5em;" class="d-flex flex-column align-items-center" id="Jeudi"></div>
+                <div style="padding-bottom: 1em; background-color:#C7C7C7; flex: 1; flex-grow: 1; row-gap: 0.5em;" class="d-flex flex-column align-items-center" id="Vendredi"></div>
+                <div style="padding-bottom: 1em; background-color:#dedede; flex: 1; flex-grow: 1; row-gap: 0.5em;" class="d-flex flex-column align-items-center" id="Samedi"></div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+HTML);
 echo $webPage->toHTML();
