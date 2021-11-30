cf06bbe14d6f6d01171359731b24a160246d8d4f
Nico <nicolas.gay@etudiant.univ-reims.fr>
2021-11-30 13:34:38 +0100

    Modification de la page prise de rdv

--- prisederdv.php
+++ prisederdv.php
@@ -66,9 +66,14 @@ $vetosSelect = "<select id='vetoId' name='vetoId'>$vetoOptions</select>";
 
 $webPage->appendJs(<<<JS
 
-    var animalList = {};
-
     window.onload = function() {
+    
+        var animalList = {};
+        
+        let animalSelect = document.getElementById('animalId');
+        let speciesSelect = document.getElementById('speciesId');
+        let vetoSelect = document.getElementById('vetoId');
+        
         new AjaxRequest(
         {
             url: "api/getUserAnimals.php",
@@ -92,10 +97,6 @@ $webPage->appendJs(<<<JS
             }
         });
         
-        let animalSelect = document.getElementById('animalId');
-        let speciesSelect = document.getElementById('speciesId');
-        let vetoSelect = document.getElementById('vetoId');
-        
         animalSelect.onchange = function(){
             if(this.options[this.selectedIndex].value == '-1')
                 speciesSelect.disabled = false;
@@ -112,7 +113,71 @@ $webPage->appendJs(<<<JS
         }
         
         vetoSelect.onchange = function() {
-            
+            reloadAvailableTimeSlot();
+        }
+        
+        let radios = document.getElementsByName('timeSlotTypeId');
+        for(let i = 0; i < radios.length; i++){
+            let radio = radios[i];
+            radio.onclick = function() {
+                reloadAvailableTimeSlot();
+            };
+        }
+        
+        
+        function clearAvailableTimeSlot(){
+            let ids = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+            for(let id in ids){
+                let meetingDiv = document.getElementById(ids[id]);
+                while(meetingDiv.firstChild){
+                    meetingDiv.removeChild(meetingDiv.firstChild);
+                }
+            }
+            console.log("REMOVE");
+        }
+        
+        function addAvailableTimeSlot(res){
+            for(let i = 0; i < res.length; i++){
+                let radio = document.createElement('radio');
+                radio.className = 'buttonHr';
+                radio.name = 'timeSlotId';
+                radio.value = res[i]['timeSlotId'];
+                radio.style.background = '#81CB45';
+                radio.innerText = res[i]['startHour'];
+                
+                console.log(res[i]['dayName']);
+                let div = document.getElementById(res[i]['dayName']);
+                document.getElementById(res[i]['dayName']).appendChild(radio);
+            }
+        }
+        
+        function reloadAvailableTimeSlot() {
+            let meetingType = document.querySelector('input[name="timeSlotTypeId"]:checked').id;
+            let vetoId = vetoSelect.options[vetoSelect.selectedIndex].value;
+            let year = '2021';
+            let week = '48';
+            console.log(meetingType);
+            console.log(vetoId);
+            console.log(year);
+            console.log(week);
+            new AjaxRequest({
+                url: "api/getAvailableTimeSlot.php",
+                method: 'get',
+                handleAs: 'json',
+                parameters: {
+                    meetingType: meetingType,
+                    vetoId: vetoId,
+                    year: year,
+                    week: week
+                },
+                onSuccess: function (res) {
+                    clearAvailableTimeSlot();
+                    addAvailableTimeSlot(res);
+                    console.log(res);
+                },
+                onError: function (status, message) {
+                }  
+            });
         }
         
     }
@@ -120,43 +185,27 @@ JS);
 
 //PLANNING
 $planning = <<<HTML
-<div class="d-flex flex-column">
+<div class="d-flex flex-column" style="width: 800px;">
     <div class="d-flex justify-content-center" style="background-color: #242424; color: white; padding-top: 10px; padding-bottom: 10px; border-radius: 10px;">Du 01/10/2021 au 06/10/2021</div>
-    <table>
-        <colgroup>
-            <col style="background-color:#C7C7C7;">
-            <col span="1">
-            <col style="background-color:#C7C7C7;">
-            <col span="1">
-            <col style="background-color:#C7C7C7;">
-        </colgroup>
-        <thead>
-            <tr>
-                <th scope="col">Lundi</th>
-                <th scope="col">Mardi</th>
-                <th scope="col">Mercredi</th>
-                <th scope="col">Jeudi</th>
-                <th scope="col">Vendredi</th>
-                <th scope="col">Samedi</th>
-            </tr>
-        </thead>
-        <tbody>
-HTML;
-
-$horaires = ['8h00 - 9h00', '9h00 - 10h00', '10h00 - 11h00', '11h00 - 12h00', '12h00 - 13h00', '13h00 - 14h00', '14h00 - 15h00', '15h00 - 16h00', '16h00 - 17h00', '17h00 - 18h00', '18h00 - 19h00'];
-$color  = "#81CB45";
-foreach ($horaires as $horaire)
-{
-    $planning .= "<tr>";
-    for ($i=0; $i<6; $i++)
-    {
-        $planning .= "<td><button class='buttonHr' type='submit' value='' style='background: $color;'>$horaire</button></td>";
-    }
-    $planning.="</tr>";
-}
-$planning.= <<< HTML
-    </tbody>
-    </table>
+    
+    <div class="d-flex flex-column justify-content-center align-items-center" style="margin-right: 25px; margin-left: 25px;">
+         <div class="d-flex justify-content-center align-items-center" style="flex: 1; flex-grow: 1; width: 100%;">
+            <span style="background-color:#C7C7C7; flex: 1; flex-grow: 1; text-align: center;">Lundi</span>
+            <span style="flex: 1; flex-grow: 1; text-align: center;">Mardi</span>
+            <span style="background-color:#C7C7C7; flex: 1; flex-grow: 1; text-align: center;">Mercredi</span>
+            <span style="flex: 1; flex-grow: 1; text-align: center;">Jeudi</span>
+            <span style="background-color:#C7C7C7; flex: 1; flex-grow: 1; text-align: center;">Vendredi</span>
+            <span style="flex: 1; flex-grow: 1; text-align: center;">Samedi</span>
+        </div>
+        <div class="d-flex justify-content-center align-items-center">
+            <div id="Lundi" style="background-color:#C7C7C7;"></div>
+            <div id="Mardi"></div>
+            <div id="Mercredi" style="background-color:#C7C7C7;"></div>
+            <div id="Jeudi"></div>
+            <div id="Vendredi"></div>
+            <div id="Samedi"></div>
+        </div>
+    </div>
 </div>
 HTML;
 //FIN PLANNING
@@ -177,6 +226,7 @@ $pageContent = <<< HTML
             <span style="font-size: 1.25em;">Vétérinaire</span>
             $vetosSelect
         </div>
+        $hiddenAnimalId
         <div class="d-flex flex-column">
             <span style="font-size: 1.25em;">Lieu</span>
             <div class="d-flex justify-content-center align-items-center" style="column-gap: 0.25em">
