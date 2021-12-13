7eb21e7a53a9407968c890d1cbddfe53c3df4c78
lege0053 <marine.legendre@etudiant.univ-reims.fr>
2021-12-13 23:55:12 +0100

    ajout d'un formulaire pour ajouter un animal

--- fiche_client_animal.php
+++ fiche_client_animal.php
@@ -278,12 +278,50 @@ try {
     $webPage->appendContent($html);
 }catch (exception $e) {
     $html .= <<<HTML
-    <div class="d-flex flex-column">
+    <div class="d-flex flex-column" style="margin-top: 40px;">
         <div class="d-flex justify-content-center" style="background-color: #262626; border-radius: 10px; width: 45%; align-self: center;">
             <h3 style="font-weight: bold;background-color: #262626; color: white; font-size: 25px; margin: auto; padding: 15px;">Ajouter un Nouvel Animal</h3> 
         </div>
-        <div class="d-flex" style="height: 200px;"></div>
+        <div class="d-flex flex-row justify-content-center mt-4 mb-5">
+    <div class="d-flex flex-column pt-2 pb-2 pr-5 pl-5" style="background-color: #DDDDDD; border-radius: 10px">
+            <form action="trmt/ajouterAnimal_trmt.php" method="post" onsubmit="hash512();">              
+                <div class="form-group d-flex flex-column">
+                    <div class="d-flex flex-row">
+                        {$webPage->getIcon("cat")}
+                        <div style="font-weight: bold;">Nom</div>
+                    </div>
+                    <input type="text" id="nom" name="nom" class="pt-1 pb-1 pr-2 pl-2 rounded" style="outline: 0; border:0;background-color: #C9C9C9;" placeholder="Nom" required>
+                </div>
+                    
+                <div class="form-group d-flex flex-column">
+                    <div class="d-flex flex-row">
+                        {$webPage->getIcon("cat")}
+                        <div style="font-weight: bold;">Date de naissance</div>
+                    </div>
+                    <input type="date" id="birth" name="birth" class="pt-1 pb-1 pr-2 pl-2 rounded" style="outline: 0; border:0;background-color: #C9C9C9;" placeholder="Date de naissance" required>
+                </div>
+                <div class="form-group d-flex flex-column">
+                    <div style="font-weight: bold;" class="d-flex flex-row">
+                        {$webPage->getIcon("cat")}
+                        <div style="font-weight: bold;">Race</div>
+                    </div>
+                    <input type="text" id="race" name="race" class="pt-1 pb-1 pr-2 pl-2 rounded" style="outline: 0; border:0;background-color: #C9C9C9;" placeholder="Race" required>
+                </div>
+                <div class="form-group d-flex flex-column">
+                    <div class="d-flex flex-row">
+                        {$webPage->getIcon("cat")}
+                        <div style="font-weight: bold;">Commentaire</div>
+                    </div>
+                    <input type="text" id="comment" name="comment" class="pt-1 pb-1 pr-2 pl-2 rounded" style="outline: 0; border:0;background-color: #C9C9C9;" placeholder="Commentaire">
+                </div>
+                <div class="d-flex flex-row justify-content-center">
+                    <div class="form-group d-inline-flex">
+                        {$webPage->getHTMLButton(true, "Ajouter")}
+                    </div>
+                </div>
+            </form>
     </div>
+</div>
 HTML;
     $webPage->appendContent($html);
 }
