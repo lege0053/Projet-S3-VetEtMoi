ad22ad612a867d528df2381dbdfab0b4743fc32f
lege0053 <marine.legendre@etudiant.univ-reims.fr>
2021-12-13 22:52:34 +0100

    Ajout de getters et setters

--- src/Vaccine.php
+++ src/Vaccine.php
@@ -7,10 +7,51 @@ class Vaccine
     private string $idSpecies;
     private string $vaccineName;
 
-    public function __construct($idVaccine, $idSpecies, $vaccineName)
+    /**
+     * @return string
+     */
+    public function getIdVaccine(): string
+    {
+        return $this->idVaccine;
+    }
+
+    /**
+     * @param string $idVaccine
+     */
+    public function setIdVaccine(string $idVaccine): void
     {
         $this->idVaccine = $idVaccine;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIdSpecies(): string
+    {
+        return $this->idSpecies;
+    }
+
+    /**
+     * @param string $idSpecies
+     */
+    public function setIdSpecies(string $idSpecies): void
+    {
         $this->idSpecies = $idSpecies;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVaccineName(): string
+    {
+        return $this->vaccineName;
+    }
+
+    /**
+     * @param string $vaccineName
+     */
+    public function setVaccineName(string $vaccineName): void
+    {
         $this->vaccineName = $vaccineName;
     }
 
