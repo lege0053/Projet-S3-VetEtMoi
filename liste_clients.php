e19329c8a11349aa504819ec085e49f1726ec168
FOSSIER Lucie <lucie.fossier@etudiant.univ-reims.fr>
2021-12-13 23:52:31 +0100

    listes des client preque

--- liste_clients.php
+++ liste_clients.php
@@ -1,6 +1,12 @@
 <?php
 declare(strict_types=1);
 require "autoload.php";
+
+$auth = new SecureUserAuthentication();
+if(!SecureUserAuthentication::isUserConnected()) header("Location: connexion.php");
+$user = $auth->getUser();
+if(!$user->isVeto()) header('Location: accueil.php');
+
 $webPage = new WebPage("liste_clients");
 
 $html= <<< HTML
@@ -12,7 +18,7 @@ $html= <<< HTML
             <label for='search-bar' style="margin: 0; font-size: 1.35em;">Nom:</label> 
             <input type="search" id="search-bar" placeholder="Rechercher..." style="padding: 10px; border-radius: 10px; width: 300px;"> 
         </div>
-        <div id="liste-clients"></div>
+        <div id="liste-clients" style='padding:20px; width:100%;' class="d-flex justify-content-center align-items-center flex-column flex-grow-1"></div>
         
     </div>
 
@@ -27,14 +33,16 @@ $webPage->appendJs(<<<JS
                 parameters: {},
                 onSuccess: function (res) {
                     let listeCli = document.getElementById('liste-clients');
-                    for(let i = 0; i < listeCli.length; i++)
+                    for(let i = 0; i < res.length; i++)
                     {
                         let clientName = document.createElement('span');
-                        clientName.style.width = '800px';
-                        clientName.style.backgroundColor = '#DDDDDD';
-                        clientName.style.padding = '25px';
+                        clientName.style.width = '100%';
+                        clientName.style.backgroundColor = '#C9C9C9';
+                        clientName.style.padding = '15px';
                         clientName.style.borderRadius = '20px';
-                        clientName.innerText = res[i]['lastName'] + " " + res[i]['firstName'];
+                        clientName.style.margin = '10px';
+                        let prenom = res[i]['firstName'];
+                        clientName.innerText = res[i]['lastName'].toUpperCase() + " " + prenom.charAt(0).toUpperCase() + prenom.slice(1).toLowerCase();
                         clientName.onclick = function(){
                             location.href = 'fiche_client_animal.php?userId=' + res[i]['userId'] + "&animalId=null";
                             
