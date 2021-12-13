59a868e5d79e5f1a25a785a39bf90d21bc719ecb
FOSSIER Lucie <lucie.fossier@etudiant.univ-reims.fr>
2021-12-13 23:20:26 +0100

    liste-clients

--- liste_clients.php
+++ liste_clients.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 require "autoload.php";
-$webPage = new WebPage("Conseils");
+$webPage = new WebPage("liste_clients");
 
 $html= <<< HTML
 <div class='d-flex align-items-center flex-column ' style='row-gap:2em; width:100%; margin-top: 2.5em;'>
@@ -13,6 +13,7 @@ $html= <<< HTML
             <input type="search" id="search-bar" placeholder="Rechercher..." style="padding: 10px; border-radius: 10px; width: 300px;"> 
         </div>
         <div id="liste-clients"></div>
+        
     </div>
 
 </div>
@@ -23,10 +24,23 @@ $webPage->appendJs(<<<JS
                 url: "api/getListClients.php",
                 method: 'get',
                 handleAs: 'json',
-                parameters: {
-                },
+                parameters: {},
                 onSuccess: function (res) {
-                    
+                    let listeCli = document.getElementById('liste-clients');
+                    for(let i=0 ; i<listeCli.length ; i++)
+                    {
+                        let clientName = docunment.createElement('span');
+                        clientName.style.width = '800px';
+                        clientName.style.backgroundColor = '#DDDDDD';
+                        clientName.style.padding = '25px';
+                        clientName.style.borderRadius = '20px';
+                        clientName.innerText = res[i]['lastName'] + " " + res[i]['firstName'];
+                        clientName.onclick = function(){
+                            location.href = 'fiche_client_animal.php?userId=' + res[i]['userId'] + "&animalId=null";
+                            
+                        }
+                        listeCli.appendChild(clientName);
+                    }  
                 },
                 onError: function (status, message) {
                 }  
