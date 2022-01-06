3350bbae877847f9cfd0e635fdaf3e3768a09af5
Nico <nicolas.gay@etudiant.univ-reims.fr>
2022-01-06 19:06:08 +0100

    Modification

--- conseils.php
+++ conseils.php
@@ -7,11 +7,11 @@ $html= <<< HTML
 <img src="img/animal/chat-chien.png" class="" style="max-width:100%;" alt="">
 
 <div style="display: flex; flex-direction: column; margin: 30px; row-gap: 30px;">
-    <div class="d-flex justify-content-center align-items-center" style="background: #F3F4F5; border-radius: 26px; padding: 25px;">
+    <div class="d-flex justify-content-center align-items-center" style="column-gap: 25px; background: #F3F4F5; border-radius: 26px; padding: 25px;">
         <img src="img/animal/cat2.png" style="width: 200px;" alt="">
         <div class="d-flex flex-column align-items-center">
             <h2 style="font-weight: bold;">Conseil pour la santé de votre chat</h2>
-            <ul class="" style="font-weight: bold;list-style-type: none;background: #DDDDDD;border-radius: 20px;padding:10px 50px;">
+            <ul class="" style="font-weight: bold;list-style-type: none;background: #DDDDDD;border-radius: 20px; padding: 15px;">
                 <li style=""> Vaccination : 
                     <span style="font-weight: normal;"> la vaccination contre le typhus (T) et le coryza (C), recommandée même pour les chats qui restent en appartement, reste nécessaire tous les ans. En revanche, la leucose (L) (sorte de SIDA chez le chat) et la rage (R) ne sont à faire que tous les 2 ans.</pan> 
                 </li>
@@ -25,12 +25,12 @@ $html= <<< HTML
             <a href="https://example.com">Suivez ce lien pour voir tous les conseils</a>
         </div>
     </div>
-    <div class="d-flex justify-content-center align-items-center" style="background: #F3F4F5; border-radius: 26px; padding: 25px;">
+    <div class="d-flex justify-content-center align-items-center" style="column-gap: 25px; background: #F3F4F5; border-radius: 26px; padding: 25px;">
         <img src="img/animal/chienConseil3.png" style="height: auto; width: 200px;" alt="">
         <div class="d-flex flex-column align-items-center">
             <h2 style="font-weight: bold;">Conseil pour la santé de votre chien</h2>
 
-            <ul style="font-weight: bold;list-style-type: none;background: #DDDDDD;border-radius: 20px;padding:10px 50px;">
+            <ul style="font-weight: bold;list-style-type: none;background: #DDDDDD;border-radius: 20px; padding: 15px;">
                 <li> Vaccination :
                     <span style="font-weight: normal;">maladie de Carré,Parvovirose,leptospirose ictérigène,Hépatite contagieuse,Toux des chenils,Rage...</span>
                 </li>
@@ -49,11 +49,11 @@ $html= <<< HTML
         </div>
     </div>
 
-    <div class="d-flex justify-content-center align-items-center" style="background: #F3F4F5; border-radius: 26px; padding: 25px;">
+    <div class="d-flex justify-content-center align-items-center" style="column-gap: 25px; background: #F3F4F5; border-radius: 26px; padding: 25px;">
         <img src="img/animal/hamster2.png" style="width: 200px;" alt="">
         <div class="d-flex flex-column align-items-center">
             <h2 style="font-weight: bold;">Conseil pour la santé de votre NAC</h2>
-            <ul style="font-weight: bold;list-style-type: none;background: #DDDDDD;border-radius: 20px;padding:10px 50px;">
+            <ul style="font-weight: bold;list-style-type: none;background: #DDDDDD;border-radius: 20px; padding: 15px;">
                 <span style="font-weight: normal;">Dans cette rubrique, vous trouverez des renseignements 
                 sur les différents soins à apporter à votre animal, ainsi que différents conseils pour prendre soins d’eux. 
                 Cet espace est accessible à tous les propriétaires, n’hésitez pas à partager.</span>
