010ae7b2cf6c909259f65753885d69a3c9f14e9f
lege0053 <marine.legendre@etudiant.univ-reims.fr>
2022-01-06 23:42:13 +0100

    suppression du s de chiens

--- accueil.php
+++ accueil.php
@@ -32,7 +32,7 @@ $html= <<< HTML
     <div class="d-flex flex-column text-center" style="margin-top: 350px;">
         <h1 style="font-weight: bold; color: white; font-size: 64px; margin-bottom: 80px;">Espace</h1>
         <div class="d-flex flex-row mt-3">
-            <a href="#">{$webPage->getImgCarre("Chien", "Chiens")}</a> 
+            <a href="#">{$webPage->getImgCarre("Chien", "Chien")}</a> 
             <a href="#">{$webPage->getImgCarre("Chat", "Chat")}</a>
             <a href="#">{$webPage->getImgCarre("nac", "NAC")}</a>
         </div>
